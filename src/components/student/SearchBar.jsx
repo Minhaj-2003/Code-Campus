47daa3301c7364af85b728709b0f2a0ffbc8b9d9
Delta student <mdminhaj005chakia@gmail.com>
2025-09-05 01:54:56 +0530

    Add course list, footer and so more

--- src/components/student/SearchBar.jsx
+++ src/components/student/SearchBar.jsx
@@ -1,27 +1,38 @@
 import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
-import { useNavigate } from 'react-router-dom'
-
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = ({data}) => {
 
-   const navigate = useNavigate()
-    const [input,setInput] = useState(data ? data : "")
-  
-    const onSearchHandler = (e)=>{
-  
-      e.preventDefault();
-      navigate('/corse-list/' + input)
+  const navigate = useNavigate();
+  const [input, setInput] = useState(data ? data : '');
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+    if (input) {
+      navigate(`/course-list/${input}`);
+    } else {
+      navigate('/course-list');
     }
+  } 
+
 
   return (
-    
-      <form action="" onSubmit={onSearchHandler} className='max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded'>
-        <img src={assets.search_icon} alt="searchicon"  className='md:w-auto w-10 px-3'/>
-        <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Search for courses' className='w-full h-full outline-none text-gray-500/80'/>
-        <button type='submit' className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1'>Search</button>
-      </form>
-   
+    <form onSubmit={onSearchHandler} className='max-w-xl w-full h-12 flex items-center bg-white rounded-full shadow-md px-3 relative'>
+      <img src={assets.search_icon} alt="search_icon" className='md:w-auto w-10 px-3' />
+      <input 
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+        type="text" 
+        placeholder='Search for courses' 
+        className='w-full h-full outline-none text-gray-500/80 rounded-full px-4' 
+      />
+      <button 
+        type='submit' 
+        className='bg-blue-600 text-white rounded-full md:px-8 px-6 md:py-3 py-2 absolute right-0 top-1/2 transform -translate-y-1/2'>
+        Search
+      </button>
+    </form>
   )
 }
 
